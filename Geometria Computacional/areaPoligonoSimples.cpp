63234c0de1bcff8d64a4ebec83112aa24f07796a
em4noel-dev <emanoel_sgs16@hotmail.com>
2022-07-18 17:07:59 -0300

    first commit

---
+++ Geometria Computacional/areaPoligonoSimples.cpp
@@ -0,0 +1,42 @@
+/*
+# Descrição:
+A função polygonArea retorna a área de um polígono simples dada as coordenadas
+ordenadas dos seus n vértices. Aqui ordenado significa que as coordenadas são 
+dadas no sentido horário ou anti-horário do primeiro ao último vértice. O algoritmo
+utiliza a "Shoelace formula".
+
+# Parâmetros: 
+vector<pair<ld, ld>> pontos -> Vector contendo todos os n vértices. first: coordenada x ; second: coordenada y
+
+# Complexidade:
+O(n)
+
+# Referência:
+https://www.geeksforgeeks.org/area-of-a-polygon-with-given-n-ordered-vertices/
+
+# Problemas:
+https://www.eolymp.com/en/problems/60
+*/
+
+#include <bits/stdc++.h>
+typedef long double ld;
+using namespace std;
+
+ld polygonArea(vector<pair<ld, ld>> pontos)
+{
+    ld area = 0.0;
+    int n = pontos.size(), j = n - 1;
+    for (int i = 0; i < n; i++)
+    {
+        area += (pontos[j].first + pontos[i].first) * (pontos[j].second - pontos[i].second);
+        j = i;
+    }
+    return abs(area / 2.0);
+}
+
+int main()
+{
+    vector<pair<ld, ld>> pontos{make_pair(0, 1.5), make_pair(2, 3), make_pair(4, 7)};
+    cout << polygonArea(pontos) << endl;
+    return 0;
+}
---
+++ Strings/totalOccurrencesSubString.cpp
@@ -0,0 +1,74 @@
+/*
+
+Referência:
+https://www.geeksforgeeks.org/frequency-substring-string/
+
+*/
+
+#include <bits/stdc++.h>
+typedef long long int lli;
+using namespace std;
+
+void computeLPSArray(string pat, int M, int lps[])
+{
+    int len = 0;
+    int i = 1;
+    lps[0] = 0;
+
+    while (i < M) 
+        if (pat[i] == pat[len]) 
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+            if (len != 0) 
+                len = lps[len - 1];
+            else
+            {
+                lps[i] = len;
+                i++;
+            }
+}
+  
+int KMPSearch(string pat, string txt)
+{
+    int M = pat.length();
+    int N = txt.length();
+    int lps[M];
+    int j = 0; 
+    computeLPSArray(pat, M, lps);
+    int i = 0;
+    int res = 0;
+    int next_i = 0;
+  
+    while (i < N)
+    {
+        if (pat[j] == txt[i])
+        {
+            j++;
+            i++;
+        }
+
+        if (j == M)
+        {
+            j = lps[j - 1];
+            res++;
+        }
+        else if (i < N && pat[j] != txt[i])
+        {
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    cout << KMPSearch("luiz", "luizabcluizabcluizabc") << endl;
+    return 0;
+}
